b4f68f7ca2f95a645f02baf6c88248efb26e99e2
Dewitt Chavez <dewittchavez@ravn.co>
2024-07-03 10:08:20 -0500

    first commit

---
+++ infrastructure/db-setup-lambda/src/main/java/com/dewitt/App.java
@@ -0,0 +1,13 @@
+package com.dewitt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
